e9b6902ecc8bcfbd857560f23214dae83fd92f30
NAGARJUN V <48723127+nagarjun063@users.noreply.github.com>
2019-04-22 18:08:21 +0530

    Add files via upload

--- PROJECT/SCRIPTS/scripts.js
+++ PROJECT/SCRIPTS/scripts.js
@@ -2,7 +2,9 @@ new Vue(
 {
    el: '#simpleApp',
     data: {
-        someText: 'hi how are you'
+        someText: 'hi how are you',
+        dropdown_font: ['Student Loan', 'Personal Loan', 'Home Loan', 'Vehicle Loan'],  
+        
     }
 }
 );
