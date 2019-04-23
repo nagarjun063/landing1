582df4e4c7892ec43464af74cab0facbf8b8e04c
NAGARJUN V <48723127+nagarjun063@users.noreply.github.com>
2019-04-23 19:00:16 +0530

    Add files via upload

--- PROJECT/SCRIPTS/scripts.js
+++ PROJECT/SCRIPTS/scripts.js
@@ -3,7 +3,15 @@ new Vue(
    el: '#simpleApp',
     data: {
         someText: 'hi how are you',
-        dropdown_font: ['Student Loan', 'Personal Loan', 'Home Loan', 'Vehicle Loan'],  
+        dropdown_font: ['Student Loan', 'Personal Loan', 'Home Loan', 'Vehicle Loan'],
+         icons: [
+      'fab fa-facebook',
+      'fab fa-twitter',
+      'fab fa-google-plus',
+      'fab fa-linkedin',
+      //'fab fa-instagram'
+      'fab fa-apple'
+]  
         
     }
 }
